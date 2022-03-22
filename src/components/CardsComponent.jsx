3a6dd8455b04991784fbf14fc3c89f5e0329ebf6
baran beyazıt <byztmain2121@gmail.com>
2022-03-22 23:09:51 +0300

    project is finished

--- src/components/CardsComponent.jsx
+++ src/components/CardsComponent.jsx
@@ -1,7 +1,9 @@
 
 import { İtemsCards,İmg,İnnerDiv,
-    P,Button
+    P,Button1,Button2,
 } from "../style/CardsComponentStyled";
+
+import styles from '../style/General.module.css'
 
 import React,{useEffect,useState} from 'react'
 import { useDispatch,useSelector } from 'react-redux';
@@ -17,7 +19,7 @@ const CardsComponent = (props) => {
     {
         if(mainMoney >= props.data.fiyat){
                 dispatch(changeMainMoneyNegative(props.data.fiyat))
-                 dispatch(changePiecePozitif([props.data.id,'kerem']))  
+                 dispatch(changePiecePozitif([props.data.id,props.data.listofreceipt]))  
         }else{
 
         }
@@ -43,9 +45,9 @@ dispatch(changeMainMoneyPozitif(props.data.fiyat))
     <P>{props.data.title}</P>
     <P>{(props.data.fiyat).toLocaleString('ar-US')} $</P>
     <İnnerDiv>
-    <Button onClick={plusButton}>+</Button>
+    <Button1 onClick={plusButton}>Buy</Button1>
     <P>{props.data.aded}</P>
-    <Button onClick={negativeButton}>-</Button>
+    <Button2 className={props.data.aded == 0 ? styles.ButtonOpactiy : styles.ButtonOpactiy2} onClick={negativeButton}>Sell</Button2>
     </İnnerDiv>
 </div>
         </İtemsCards>
--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,14 +1,55 @@
 
+import {useState} from 'react';
 
-import {Wrapper
+import {Wrapper,P_foter
 } from '../style/FooterComponentStyled'
+import styles from '../style/General.module.css'
+import { useSelector } from 'react-redux';
+import {selectİtems,selectMoney
+} from '../redux/cardSlice'
+import FooterİnnerComponent from './FooterİnnerComponent';
+
 
 const FooterComponent = () => {
+
+
+
+    // itemları getirtir.
+    const getİtemsData = useSelector(selectİtems);
+    // itemlerin listesini
+    const getMoneyData = useSelector(selectMoney);
+
+
+
     return ( 
         <Wrapper>
-            lkj
+  <div className={styles.footerWrapper}>
+  <p className={styles.p_wrapperr}>TİTLE</p> 
+<p className={styles.p_wrapperr}>ADED</p>
+<p className={styles.p_wrapperr}>TOPLAM FİYAT</p>
+
+  </div>
+  
+                  {
+getİtemsData.map((dataf) => (
+    <FooterİnnerComponent data={dataf} key={dataf.id} />
+))
+}   
+<P_foter> Harcanan toplam: {(187000000000-getMoneyData).toLocaleString('ar-US')}</P_foter>
         </Wrapper>
      );
 }
  
 export default FooterComponent;
+
+// {(props.data.aded*props.data.fiyat).toLocaleString('ar-US')}
+// getMoneyData
+
+
+/*
+
+const [kissadan,useKisseden] = useState(null);
+useKisseden(187000000000-getMoneyData)
+
+
+*/
